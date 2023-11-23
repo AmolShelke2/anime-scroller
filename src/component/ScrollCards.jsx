880a032daa0993ed6c562a84ec7a8d84b931ec9f
Amol Shelke <shelkeamol897@gmail.com>
2023-11-23 19:49:41 +0530

    Update ScrollCards.jsx

--- src/component/ScrollCards.jsx
+++ src/component/ScrollCards.jsx
@@ -1,27 +1,21 @@
-import React, { useRef, useState } from "react";
+import React, { useRef } from "react";
 import { ScrollCard } from "./ScrollCard";
 import { FiArrowLeftCircle, FiArrowRightCircle } from "react-icons/fi";
 import { animeData } from "../lib/data";
 
 export const ScrollCards = () => {
-  const [left, setLeft] = useState(false);
-  const [right, setRight] = useState(true);
   const ref = useRef(null);
 
   const scroll = (scrollOffset) => {
     ref.current.scrollLeft += scrollOffset;
-    if (ref.current.scrollLeft + scrollOffset >= ref.current.scrollLeftMax)
-      return setRight(false);
-    if (ref.current.scrollLeft + scrollOffset < 0) return setLeft(false);
-    if (left !== true) setLeft(true);
-    if (right !== true) setRight(true);
   };
 
   return (
     <div className="px-4 relative">
       <div
         className="grid  grid-flow-col gap-[10px] py-[25px] overflow-x-scroll snap-x cards w-full scroll-smooth"
-        ref={ref}>
+        ref={ref}
+      >
         {animeData.map((anime) => (
           <ScrollCard
             key={anime.title}
